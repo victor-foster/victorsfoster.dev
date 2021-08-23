ada432b8e20f13900fd7035486f8fd50bc376e4d
Victor Foster <vfoster@inspirato.com>
2021-08-23 13:51:34 -0600

    Made text updates

--- theme.config.js
+++ theme.config.js
@@ -3,14 +3,14 @@ const YEAR = new Date().getFullYear();
 const myBlogTheme = {
 	footer: (
 		<footer>
-			<dl>
+			{/* <dl>
 				<dt>Twitter</dt>
 				<dd>[@victor_foster](https://twitter.com/victor_foster)</dd>
 				<dt>GitHub</dt>
 				<dd>[@victor-foster](https://github.com/victor-foster)</dd>
 				<dt>Email</dt>
 				<dd>[victor.foster@gmail.com](mailto:victor.foster@gmail.com)</dd>
-			</dl>
+			</dl> */}
 		</footer>
 	),
 };
