f56232b6e10fd2445ba80368024f4a0eec83a1a7
Victor Foster <vfoster@inspirato.com>
2021-08-23 14:09:01 -0600

    Added purge css

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 	enabled: process.env.ANALYZE === 'true',
 });
 
+const withPurgeCss = require('next-purgecss');
+
 const withNextra = require('nextra')('nextra-theme-blog', './theme.config.js');
 
-module.exports = withBundleAnalyzer(withNextra());
+module.exports = withPurgeCss(withBundleAnalyzer(withNextra()));
