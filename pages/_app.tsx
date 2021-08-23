f9091a854efbcd2852c6d8832f2a0f2d01c16059
Victor Foster <vfoster@inspirato.com>
2021-08-23 14:57:40 -0600

    Removed dead code

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,21 +5,8 @@ import '../styles/main.scss';
 import React, { Component, useEffect } from 'react';
 import type { AppProps, NextWebVitalsMetric } from 'next/app';
 import Head from 'next/head';
-import * as gtag from '../lib/gtag';
-import { useRouter } from 'next/router';
 
 function MyPersonalWebsite({ Component, pageProps }: AppProps) {
-	const router = useRouter();
-	useEffect(() => {
-		const handleRouteChange = (url: string) => {
-			gtag.pageview(url);
-		};
-		router.events.on('routeChangeComplete', handleRouteChange);
-		return () => {
-			router.events.off('routeChangeComplete', handleRouteChange);
-		};
-	}, [router.events]);
-
 	return (
 		<>
 			<Head>
@@ -29,17 +16,5 @@ function MyPersonalWebsite({ Component, pageProps }: AppProps) {
 		</>
 	);
 }
-
-// export function reportWebVitals(metric: NextWebVitalsMetric) {
-// 	// Use `window.gtag` if you initialized Google Analytics as this example:
-// 	// https://github.com/vercel/next.js/blob/canary/examples/with-google-analytics/pages/_document.js
-// 	// window?.gtag('event', name, {
-// 	// 	event_category: label === 'web-vital' ? 'Web Vitals' : 'Next.js custom metric',
-// 	// 	value: Math.round(name === 'CLS' ? value * 1000 : value), // values must be integers
-// 	// 	event_label: id, // id unique to current page load
-// 	// 	non_interaction: true, // avoids affecting bounce rate.
-// 	// });
-// 	console.log(metric);
-// }
 
 export default MyPersonalWebsite;
