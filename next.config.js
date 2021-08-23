5760c5978d9ade22fcc037d8a2aec4ae94aac077
Victor Foster <vfoster@inspirato.com>
2021-08-23 14:37:45 -0600

    Made updates after code review

--- next.config.js
+++ next.config.js
@@ -2,8 +2,6 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 	enabled: process.env.ANALYZE === 'true',
 });
 
-const withPurgeCss = require('next-purgecss');
-
 const withNextra = require('nextra')('nextra-theme-blog', './theme.config.js');
 
-module.exports = withPurgeCss(withBundleAnalyzer(withNextra()));
+module.exports = withBundleAnalyzer(withNextra());
