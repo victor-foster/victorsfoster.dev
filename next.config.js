249a646e6616f846abb646390b5d1465afa89753
Victor Foster <vfoster@inspirato.com>
2021-09-04 20:08:43 -0600

    Fixed a few minor issues

--- next.config.js
+++ next.config.js
@@ -4,4 +4,15 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 
 const withNextra = require('nextra')('nextra-theme-blog', './theme.config.js');
 
-module.exports = withBundleAnalyzer(withNextra());
+const path = require('path');
+const withPlugins = require('next-compose-plugins');
+
+const nextConfig = {
+	cssModules: true,
+	cssLoaderOptions: {
+		importLoaders: 1,
+		localIdentName: '[local]-[hash:base64:5]',
+	},
+	trailingSlash: true,
+};
+module.exports = withPlugins([withBundleAnalyzer, withNextra], nextConfig);
