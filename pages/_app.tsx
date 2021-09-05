4d926da0c392416aabf18ca65724fdc4243d26af
Victor Foster <vfoster@inspirato.com>
2021-09-04 18:56:32 -0600

    Fixed google tracking script

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,12 +5,24 @@ import '../styles/main.scss';
 import React, { Component, useEffect } from 'react';
 import type { AppProps, NextWebVitalsMetric } from 'next/app';
 import Head from 'next/head';
+import Script from 'next/script';
 
 function MyPersonalWebsite({ Component, pageProps }: AppProps) {
 	return (
 		<>
 			<Head>
 				<link rel='alternate' type='application/rss+xml' title='RSS' href='/feed.xml' />
+				<Script strategy='lazyOnload' src={`https://www.googletagmanager.com/gtag/js?id=UA-29309617-1`} />
+				<Script strategy='lazyOnload'>
+					{`
+						window.dataLayer = window.dataLayer || [];
+						function gtag(){dataLayer.push(arguments);}
+						gtag('js', new Date());
+						gtag('config', 'UA-29309617-1', {
+							page_path: window.location.pathname,
+						});
+				`}
+				</Script>
 			</Head>
 			<Component {...pageProps} />
 		</>
