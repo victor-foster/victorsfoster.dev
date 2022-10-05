4ceba71ca976cdd1089ca270bc127f399b73280d
Victor Foster <victor.foster@gmail.com>
2022-10-05 12:45:56 -0400

    stubbed out the theme toggle component

---
+++ components/ThemeToggle/ThemeToggle.tsx
@@ -0,0 +1,92 @@
+import { useState } from "react";
+
+export const ThemeToggle = () => {
+  const [isShrinkView, setIsShrinkView] = useState(false);
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const handleSidebarView = () => {
+    setIsShrinkView(!isShrinkView);
+  };
+
+  const handleThemeChange = () => {
+    setIsDarkMode(!isDarkMode);
+    document.body.classList.toggle("dark");
+  };
+
+  return (
+    <div className={`sidebar-container${isShrinkView ? " shrink" : ""}`}>
+      <button
+        className="sidebar-viewButton"
+        type="button"
+        aria-label={isShrinkView ? "Expand Sidebar" : "Shrink Sidebar"}
+        title={isShrinkView ? "Expand" : "Shrink"}
+        onClick={handleSidebarView}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="feather feather-chevron-left"
+        >
+          <polyline points="15 18 9 12 15 6" />
+        </svg>
+      </button>
+      <div className="sidebar-wrapper">
+        <div className="sidebar-themeContainer">
+          <label
+            labelFor="theme-toggle"
+            className={`sidebar-themeLabel${isDarkMode ? " switched" : ""}`}
+          >
+            <input
+              className="sidebar-themeInput"
+              type="checkbox"
+              id="theme-toggle"
+              onChange={handleThemeChange}
+            />
+            <div className="sidebar-themeType light">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                className="sidebar-listIcon"
+              >
+                <circle cx="12" cy="12" r="5" />
+                <path d="M12 1v2M12 21v2M4.22 4.22l1.42 1.42M18.36 18.36l1.42 1.42M1 12h2M21 12h2M4.22 19.78l1.42-1.42M18.36 5.64l1.42-1.42" />
+              </svg>
+              <span className="sidebar-themeInputText">Light</span>
+            </div>
+            <div className="sidebar-themeType dark">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                className="sidebar-listIcon"
+              >
+                <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z" />
+              </svg>
+              <span className="sidebar-themeInputText">Dark</span>
+            </div>
+          </label>
+        </div>
+      </div>
+    </div>
+  );
+};
