b326a5d15dfbc6962b6b5795a0425aa339f14adb
Victor Foster <victor.foster@gmail.com>
2025-10-27 20:59:08 -0400

    updated meta title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,28 @@
-import 'sanitize.css';
-import 'nextra-theme-blog/style.css';
-import '../styles/main.scss';
-
-import React from 'react';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import Script from 'next/script';
+import '../styles/main.scss';
 
-const MyPersonalWebsite = ({ Component, pageProps }: AppProps) => {
+export default function App({ Component, pageProps }: AppProps) {
+	// Get the page title and summary from frontmatter (available in pageProps with Nextra)
+	const pageTitle = pageProps.title || 'Victor Foster - UI Engineer';
+	const pageDescription = pageProps.summary || 'Victor Foster - UI Engineer';
+
 	return (
 		<>
 			<Head>
-				<link rel='alternate' type='application/rss+xml' title='RSS' href='/feed.xml' />
+        <link rel='alternate' type='application/rss+xml' title='RSS' href='/feed.xml' />
+
+				<title>{pageTitle}</title>
+				<meta name='description' content={pageDescription} />
+				<meta property='og:title' content={pageTitle} />
+				<meta property='og:description' content={pageDescription} />
+				<meta property='og:site_name' content='Victor Foster - UI Engineer' />
+				<meta name='twitter:title' content={pageTitle} />
+				<meta name='twitter:description' content={pageDescription} />
+        <meta name='robots' content='follow, index' />
 			</Head>
+
 			<Script
 				strategy='lazyOnload'
 				src={`https://www.googletagmanager.com/gtag/js?id=UA-29309617-1`}
@@ -31,6 +41,4 @@ const MyPersonalWebsite = ({ Component, pageProps }: AppProps) => {
 			<Component {...pageProps} />
 		</>
 	);
-};
-
-export default MyPersonalWebsite;
+}
